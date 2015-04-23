ada405d1d7326fc7c6e95bece0c82a5fb766d129
rickydo <g3tdo@yahoo.com>
2015-04-23 18:15:37 -0500

    AR saves dob with day first month second and year last

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150423192753) do
+ActiveRecord::Schema.define(version: 20150423215152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,5 +42,13 @@ ActiveRecord::Schema.define(version: 20150423192753) do
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
   add_index "admins", ["unlock_token"], name: "index_admins_on_unlock_token", unique: true, using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "dob"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+me = Admin.create!(email: "help1week@a-time.com", password: "password", password_confirmation: "password")
+
+# user1 = User.create!(first_name: "Tara", last_name: "Tomb raider", dob: "4/10/1990")
+# user2 = User.create!(first_name: "Kevin", last_name: "Long", dob: "5/12/1986")
+# user3 = User.create!(first_name: "Johnny", last_name: "Appleseed", dob: "29/8/1981")
+# user4 = User.create!(first_name: "Kimberly", last_name: "Grave Hunter", dob: "3/11/1987")

791d7a1f07249ed48474ba6655bcb896dc7d081c
rickydo <g3tdo@yahoo.com>
2015-04-23 14:39:20 -0500

    modified admin model and migration to include confirmable, lockable, and timeout

---
+++ app/models/admin.rb
@@ -0,0 +1,7 @@
+class Admin < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :timeoutable, :lockable
+end
